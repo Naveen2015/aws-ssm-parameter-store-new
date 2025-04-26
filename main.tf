18ccc63b4a6a3eb5d435de5668cc098d5efbb42c
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-26 11:19:21 -0500

    SecureString

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_ssm_parameter" "parameter" {
 resource "aws_ssm_parameter" "passwords" {
   count = length(var.passwords)
   name  = var.passwords[count.index].name
-  type  = "SecretString"
+  type  = "SecureString"
   value = var.passwords[count.index].value
   key_id = "2c5cbdcc-7978-489b-88b7-1f2df783e6c7"
 }
